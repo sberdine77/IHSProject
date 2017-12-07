a445a20eec8272a0c6c21640461b41bd6fbaeee0
DE2i-150 <de2i-150@DE2i150-FPGA.(none)>
2017-12-07 17:27:54 -0300

    Adicionando funcionalidade de mostrar fotos (versão final/communication2.cpp)

--- communication2.cpp
+++ communication2.cpp
@@ -8,6 +8,10 @@
 #include <cstdlib>
 #include <string.h>
 #include <fcntl.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
+
 
 
 using namespace LibSerial;
@@ -24,7 +28,8 @@ struct Person {
 	char* tag;
 	char password;
 	int level;
-	char* foto;
+	ALLEGRO_BITMAP* image;
+	//char* foto;
 }pessoas[7];
 
 
@@ -42,13 +47,15 @@ char* serialRecievePrint(SerialStream* serial_port) {
 		}
 	}
 
+	ID[8] = '\0';
+
 	return ID;
 }
 
 
 int* strTo7Seg(char* string) {
-	int array1[4];
-	int array2[4];
+	//int array1[4];
+	//int array2[4];
 	int sum1 = 0, sum2 = 0;
 	int div = 4096;
 	int aux;
@@ -93,12 +100,12 @@ void print7Seg(pio k, pio j, unsigned char* hexdigit, int dev, int i, int val) {
 	write(dev, &k, 4);
 }
 
-int serialRecieve(SerialStream* serial_port)
+void serialRecieve(SerialStream* serial_port)
 {
 	using namespace std;
 	using namespace LibSerial;
 
-	char c;
+	//char c;
 	serial_port->Open("/dev/ttyUSB0");
 	if (!serial_port->good()) {
 		std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
@@ -158,6 +165,41 @@ int main(int argc, char** argv) {
 		0x7F, 0x6F, 0x77, 0x7C,
 		0x39, 0x5E, 0x79, 0x71 };
 
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_BITMAP *negado = NULL;
+
+	if(!al_init()) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+    }
+
+    if(!al_init_image_addon()) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+    }
+
+    display = al_create_display(800,600);
+
+    if(!display) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+    }	
+
+	negado = al_load_bitmap("not_pass.jpg");
+
+   if(!negado) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
+
+   //al_draw_bitmap(negado,0,0,0);
+
+   al_flip_display();
 	//cout << "abriusaporra!" << std::endl;
 
 	pio j, k;
@@ -172,31 +214,66 @@ int main(int argc, char** argv) {
 	pessoas[0].Nome = (char*)("Lucas\0");
 	pessoas[0].tag = (char*)("372F64F0\0");
 	pessoas[0].level = 3;
+	pessoas[0].image = al_load_bitmap("lucas.jpg");
 	//pessoas[0].foto = (char*)("lama2.jpeg");
+	if(!pessoas[0].image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
 
 	pessoas[1].Nome = (char*)("Thyago\0");
 	pessoas[1].tag = (char*)("A70B64F0\0");
 	pessoas[1].level = 3;
+	pessoas[1].image = al_load_bitmap("thyago.jpg");
 	//pessoas[1].foto = (char*)("thyago.jpg");
+	if(!pessoas[1].image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
 
 	pessoas[2].Nome = (char*)("Mateus\0");
 	pessoas[2].tag = (char*)("E71EB654\0");
 	pessoas[2].level = 1;
+	pessoas[2].image = al_load_bitmap("mateus.jpg");
 	//pessoas[2].foto = (char*)("mateus.jpg");
+	if(!pessoas[2].image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
 
 	pessoas[3].Nome = (char*)("Sávio\0");
 	pessoas[3].tag = (char*)("876EA2FD\0");
 	pessoas[3].level = 3;
+	pessoas[3].image = al_load_bitmap("savio.jpg");
 	//pessoas[3].foto = (char*)("savio.jpg");
+	if(!pessoas[3].image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
 
 	pessoas[4].Nome = (char*)("Carriço\0");
 	pessoas[4].tag = (char*)("278F63F0\0");
 	pessoas[4].level = 3;
+	pessoas[4].image = al_load_bitmap("carrico.jpg");
 	//pessoas[4].foto = (char*)("Daniel.jpeg");
+	if(!pessoas[4].image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
 
 	//Pessoa *pessoa1 = new Pessoa((char*)("Carriço\0"), (char*)("278F63F0\0"), '1', 1);
 
-	int x;
+	//int x;
 	int i;
 	int* var;
 	char* ID;
@@ -204,6 +281,7 @@ int main(int argc, char** argv) {
 
 	serialRecieve(serial_rfid);
 	while (1) {
+		ID = NULL;
 		ID = serialRecievePrint(serial_rfid);
 		var = strTo7Seg(ID);
 
@@ -215,13 +293,22 @@ int main(int argc, char** argv) {
 		std::cout << j.dado << std::endl;
 
 		for (i = 0; i<5; i++) {
+			//std::cout << "i= " << i << std::endl;
+			//std::cout << "tag= " << pessoas[i].tag << std::endl;
+			//std::cout << "id= " << ID << std::endl;
+
 			if (strcmp(ID, pessoas[i].tag) == 0)
 			{
+				
 				std::cout << pessoas[i].Nome << std::endl;
+				
 				
 				if (pessoas[i].level >= j.dado)
 				{
 					std::cout << "Acesso permitido" << std::endl;
+					al_clear_to_color(al_map_rgb(0,0,0));
+					al_draw_bitmap(pessoas[i].image,0,0,0);
+					al_flip_display();
 					k.pio = 3;
 					k.dado = 255;
 					write(dev, &k, 4);
@@ -242,7 +329,10 @@ int main(int argc, char** argv) {
 				}
 				else
 				{
+					al_clear_to_color(al_map_rgb(0,0,0));
 					std::cout << "Acesso negado" << std::endl;
+   					al_draw_bitmap(negado,0,0,0);
+   					al_flip_display();
 
 					k.pio = 3;
 					k.dado = 0;
@@ -257,8 +347,11 @@ int main(int argc, char** argv) {
 			}
 		}
 		if (i == 5)
-		{
+		{	
+			al_clear_to_color(al_map_rgb(0,0,0));
 			std::cout << "Acesso negado" << std::endl;
+			al_draw_bitmap(negado,0,0,0);
+   			al_flip_display();
 
 			k.pio = 3;
 			k.dado = 0;
@@ -271,5 +364,10 @@ int main(int argc, char** argv) {
 
 	}
 
+	/*al_destroy_display(display);
+	for( int z=0; z<5 ; z++){
+		al_destroy_bitmap(pessoas[z].image);
+	}	
+	al_destroy_bitmap(negado);*/
 	return 0;
 }
